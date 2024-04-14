62a739e4c533044111c38ce2c38a6d77276d3e22
AdamJaworski <jaworskiadam0203@gmail.com>
2024-04-14 19:16:09 +0200

    not as good but rn it gathers data from first sounds

--- codes.py
+++ codes.py
@@ -1,41 +1,31 @@
 import numpy as np
 import global_variables
+from math import floor
+low_v = np.array([697, 770, 852, 941])
+high_v = np.array([1209, 1336, 1477])
 
-keys = {
-    (697, 1209): '1',
-    (697, 1336): '2',
-    (697, 1477): '3',
-    (770, 1209): '4',
-    (770, 1336): '5',
-    (770, 1477): '6',
-    (852, 1209): '7',
-    (852, 1336): '8',
-    (852, 1477): '9',
-    (941, 1209): '*',
-    (941, 1336): '0',
-    (941, 1477): '#'
-}
+new_low_v  = np.array([0, 0, 0, 0])
+new_high_v = np.array([0, 0, 0])
 
 
-def get_freq(low_f, high_f) -> str:
-    low_v  = np.array([697, 770, 852, 941])
-    high_v = np.array([1209, 1336, 1477])
+def from_dict(low_f, high_f) -> str:
+    global new_low_v, new_high_v
 
-    low_min = abs(low_v - low_f)
-    high_min = abs(high_v - high_f)
+    low_min = abs(new_low_v - low_f)
+    high_min = abs(new_high_v - high_f)
 
     if np.min(low_min) > global_variables.FREQ_TOLERANCE:
         return ''
     if np.min(high_min) > global_variables.FREQ_TOLERANCE:
         return ''
 
-    low_f = low_v[np.where(low_min == np.min(low_min))][0]
-    high_f = high_v[np.where(high_min == np.min(high_min))][0]
+    low_f = new_low_v[np.where(low_min == np.min(low_min))][0]
+    high_f = new_high_v[np.where(high_min == np.min(high_min))][0]
 
-    return keys[(low_f, high_f)]
+    return global_variables.new_keys[(low_f, high_f)]
 
 
-def extract_number(audio, fs) -> str:
+def get_freq(audio, fs) -> tuple:
     N = 2 ** np.ceil(np.log2(len(audio)))
     N = int(max(N, fs / 21))
     fft_result = np.fft.fft(audio, n=N)
@@ -59,8 +49,40 @@ def extract_number(audio, fs) -> str:
         high_f = fft_freq[high_f_index]
     else:
         high_f = None
+    return high_f, low_f
 
+
+def get_code(audio, fs) -> str:
+    high_f, low_f = get_freq(audio, fs)
     if low_f and high_f:
-        return get_freq(low_f, high_f)
+        return from_dict(low_f, high_f)
     else:
         return ''
+
+
+def set_keys(audio: np.ndarray, fs: float, code: str):
+    global low_v, high_v
+    high_f, low_f = get_freq(audio, fs)
+    high_f = int(floor(high_f))
+    low_f = int(floor(low_f))
+
+    if low_f and high_f:
+        low_min = abs(low_v - low_f)
+        high_min = abs(high_v - high_f)
+
+        if np.min(low_min) > global_variables.FREQ_TOLERANCE:
+            return False
+        if np.min(high_min) > global_variables.FREQ_TOLERANCE:
+            return False
+
+        if new_low_v[np.where(low_min == np.min(low_min))] == 0:
+            new_low_v[np.where(low_min == np.min(low_min))] = low_f
+        if new_high_v[np.where(high_min == np.min(high_min))] == 0:
+            new_high_v[np.where(high_min == np.min(high_min))] = high_f
+
+        if global_variables.new_keys.get((int(new_low_v[np.where(low_min == np.min(low_min))]), int(new_high_v[np.where(high_min == np.min(high_min))]))) is not None:
+            return False
+
+        global_variables.new_keys[(int(new_low_v[np.where(low_min == np.min(low_min))]), int(new_high_v[np.where(high_min == np.min(high_min))]))] = code
+
+    return True
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 import librosa
@@ -18,7 +19,7 @@ def get_non_silent_chunks(audio: np.ndarray, fs) -> list:
     :param audio: he input audio signal.
     :return: list of audio chunks
     """
-    non_silent_intervals = librosa.effects.split(audio, top_db=-(global_variables.NOISE_THRESHOLD * 2), hop_length=global_variables.HOP, frame_length=int(fs/40))
+    non_silent_intervals = librosa.effects.split(audio, top_db=-(global_variables.NOISE_THRESHOLD * 2), hop_length=global_variables.HOP, frame_length=int(fs/20))
     audio_segments = []
     for start_idx, end_idx in non_silent_intervals:
         segment = audio[start_idx:end_idx]
@@ -32,31 +33,38 @@ def main(file: str) -> None:
     :param file: name of audio file located in ./data/
     :return: code as string
     """
-    audio, fs = librosa.load(audio_path + file)
+    audio, fs = librosa.load(file)
 
-    audio = filters.select_freq(audio, fs)
+    audio = filters.select_freq(audio, fs)     # selecting freq
 
-    audio = utilities.normalize_audio_over_time(audio, resolution=int(len(audio) / fs)) # normalizing every second
+    audio = utilities.normalize_audio_over_time(audio, resolution=int(len(audio) / fs))     # normalizing every second
 
-    while utilities.estimate_noise_level(audio) > global_variables.NOISE_THRESHOLD - 10: # Lowering ground
+    while utilities.estimate_noise_level(audio) > global_variables.NOISE_THRESHOLD:    # Lowering ground
         audio = utilities.spectral_subtraction(audio, fs)
 
-    audio = utilities.noise_ceiling(audio, fs)
+    audio = utilities.noise_ceiling(audio, fs)      # flatten top
+    audio = utilities.noise_gate(audio, fs)         # remove noisy bottom
 
-    audio = utilities.noise_gate(audio, fs)
+    audio_chunks = get_non_silent_chunks(audio, fs)
+
+    # setting code
+    codes_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#']
+    for index in range(len(codes_list)):
+        while not codes.set_keys(audio_chunks[index], fs, codes_list[index]):
+            del audio_chunks[index]
 
     audio_chunks = get_non_silent_chunks(audio, fs)
     code = ''
     for chunk in audio_chunks:
-        code += codes.extract_number(chunk, fs)
+        code += codes.get_code(chunk, fs)
     #print(code == '123456789*0#874995*888285837**40#*9915*351#043387301#149951161#978567136')
     print(code)
-    utilities.plot_volume_over_time(audio, fs)
 
 
 if __name__ == "__main__":
-    main(r'challenge 2022.wav')
-    main(rf's{9}.wav')
+    main(audio_path + r'challenge 2024.wav')
     # for i in range(10):
     #     main(rf's{i}.wav')
+    # for i in range(12):
+
 
